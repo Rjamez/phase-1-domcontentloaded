e7ad976fb3af0f30f74ab8fcc536f345ed0212dc
Rjamez <robin.adhola.student@moringaschool.com>
2024-10-08 12:29:09 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",replaceText);
+
+function replaceText(){
+    let text = document.querySelector('p')
+    text.textContent = 'This is really cool!'
+}
